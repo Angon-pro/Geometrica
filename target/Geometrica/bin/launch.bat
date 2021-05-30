db8182969e2afa9dc478adb82c6eceeb300f8fb4
Vadim <vadim.galimov34@gmail.com>
2021-05-30 23:59:30 +0300

    release v1

---
+++ target/Geometrica/bin/launch.bat
@@ -0,0 +1,7 @@
+@echo off
+(echo.set sh=CreateObject^("Wscript.Shell"^)
+echo.sh.Run """%~nx0"" 1", 0)>launch.vbs
+if "%~1"=="" (start "" "launch.vbs"&exit /b)
+set JLINK_VM_OPTIONS=
+set DIR=%~dp0
+"%DIR%\java" %JLINK_VM_OPTIONS% -m com.angon/com.angon.App %*
---
+++ target/Geometrica/bin/launch.vbs
@@ -0,0 +1,2 @@
+set sh=CreateObject("Wscript.Shell")
+sh.Run """launch.bat"" 1", 0
